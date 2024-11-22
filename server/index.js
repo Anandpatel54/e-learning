d879a99c2bae6c0b12e3218ce1be9b6c10d6c506
Anand patel <panand054@gmail.com>
2024-11-22 10:51:08 +0530

    All part done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./Database/db.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config({});
 // call datbase connection here
 connectDB();
 const app = express();
+
+// api
+app.use("/api/v1/user", userRoute);
+
+app.get("/home", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello i am comming from backend",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
