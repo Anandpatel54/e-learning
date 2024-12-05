d94d2abde8dcf215651dcca6bad34457248a6c86
Anand patel <panand054@gmail.com>
2024-12-05 12:17:04 +0530

    making some part

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,12 +4,16 @@ export const generateToken = (res, user, message) => {
   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
     expiresIn: "1d",
   });
+
   return res
     .status(200)
-    .cookie("token", {
+    .cookie("token", token, {
       httpOnly: true,
       sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000,
-    })
-    .json({ success: true, message, user });
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    }).json({
+        success:true,
+        message,
+        user
+    });
 };
