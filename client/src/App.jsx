002c7399ba8611b691518d733565d5e1dea5beeb
Anand patel <panand054@gmail.com>
2025-01-21 12:14:54 +0530

    navbar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 
 const App = () => {
   return (
     <main className="">
+      <Navbar />
       <Login />
     </main>
   );
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import { School } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const Navbar = () => {
+  const user = true;
+  return (
+    <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
+      <div className="md:flex max-w-5xl mx-auto hidden justify-between items-center gap-10 h-full">
+        <div className="flex items-center gap-2">
+          <School size={"30"} />
+          <h1 className="hidden md:block font-extrabold text-2xl">
+            E-Learning
+          </h1>
+        </div>
+        {/* user icon and dark mode icon*/}
+        <div>
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>My learning</DropdownMenuItem>
+                  <DropdownMenuItem>Edit profile</DropdownMenuItem>
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button>Signup</Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
