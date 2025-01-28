cf9ed32626a703077e21e14ed7a8afa72eb1b29f
Anand patel <panand054@gmail.com>
2025-01-28 10:37:44 +0530

    still making

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./Database/db.js";
 import userRoute from "./routes/userRoute.js";
+import courseRoute from "./routes/courseRoute.js";
 
 dotenv.config({});
 
@@ -17,13 +18,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // api
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listen at port ${PORT}`);
