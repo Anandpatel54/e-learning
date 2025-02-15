69f7b1adb508433acd1bd246139f8dc43be5f891
Anand patel <panand054@gmail.com>
2025-02-15 10:43:32 +0530

    lectureController part created

---
+++ server/controllers/lectureController.js
@@ -0,0 +1,31 @@
+import { Lecture } from "../models/lectureModel.js";
+import { Course } from "../models/courseModel.js";
+
+export const createLecture = async (req, res) => {
+  try {
+    const { lectureTitle } = req.body;
+    const { courseId } = req.params;
+    if (!lectureTitle || !courseId) {
+      return res.status(400).json({
+        message: "Lecture Title required",
+        success: false,
+      });
+    }
+    //create lecture
+    const lecture = await Lecture.create({ lectureTitle });
+    const course = await Course.findById(courseId);
+    if (course) {
+      course.lectures.push(lecture._id);
+      await course.save();
+    }
+    return res.status(200).json({
+      message: "Lecture created Successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to create lecture",
+    });
+  }
+};
